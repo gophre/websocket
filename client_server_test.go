6387ee4987041493b5739b60a037b63d81f3da3a
l3dlp <web@l3dlp.com>
2022-12-10 07:34:55 +0100

    First shot as new lib

--- client_server_test.go
+++ client_server_test.go
@@ -255,7 +255,7 @@ func TestDialCookieJar(t *testing.T) {
 		u.Scheme = "https"
 	}
 
-	cookies := []*http.Cookie{{Name: "gorilla", Value: "ws", Path: "/"}}
+	cookies := []*http.Cookie{{Name: "gophre", Value: "ws", Path: "/"}}
 	d.Jar.SetCookies(u, cookies)
 
 	ws, _, err := d.Dial(s.URL, nil)
@@ -264,18 +264,18 @@ func TestDialCookieJar(t *testing.T) {
 	}
 	defer ws.Close()
 
-	var gorilla string
+	var gophre string
 	var sessionID string
 	for _, c := range d.Jar.Cookies(u) {
-		if c.Name == "gorilla" {
-			gorilla = c.Value
+		if c.Name == "gophre" {
+			gophre = c.Value
 		}
 
 		if c.Name == "sessionID" {
 			sessionID = c.Value
 		}
 	}
-	if gorilla != "ws" {
+	if gophre != "ws" {
 		t.Error("Cookie not present in jar.")
 	}
 
